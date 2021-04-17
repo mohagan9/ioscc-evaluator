117ab992f49b2b7841f5f65b2cb90058f6770c93
GitHub Action <action@github.com>
2021-04-17 19:40:39 +0000

    Template cleanup

--- src/main/kotlin/com/github/mohagan9/iosccevaluator/MyBundle.kt
+++ src/main/kotlin/com/github/mohagan9/iosccevaluator/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.mohagan9.iosccevaluator
 
 import com.intellij.AbstractBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/mohagan9/iosccevaluator/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/mohagan9/iosccevaluator/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.mohagan9.iosccevaluator.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.mohagan9.iosccevaluator.MyBundle
 
 class MyApplicationService {
 
