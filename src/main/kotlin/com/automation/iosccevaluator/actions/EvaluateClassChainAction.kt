6522bced9cc7a47b0490b1b8f097d77236215555
mohagan9 <mohagan9@gmail.com>
2021-04-24 22:58:31 +0100

    Added evaluate iOS Class Chain option to XML Actions

---
+++ src/main/kotlin/com/automation/iosccevaluator/actions/EvaluateClassChainAction.kt
@@ -0,0 +1,10 @@
+package com.automation.iosccevaluator.actions
+
+import com.intellij.openapi.actionSystem.AnAction
+import com.intellij.openapi.actionSystem.AnActionEvent
+
+class EvaluateClassChainAction : AnAction() {
+    override fun actionPerformed(e: AnActionEvent) {
+
+    }
+}
