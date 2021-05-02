628e82c22f874639e07a3fe5980e7bb47ed2ee07
mohagan9 <mohagan9@gmail.com>
2021-05-02 22:53:29 +0100

    Removed ktlint

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,8 +16,6 @@ plugins {
     id("org.jetbrains.changelog") version "1.1.2"
     // detekt linter - read more: https://detekt.github.io/detekt/gradle.html
     id("io.gitlab.arturbosch.detekt") version "1.16.0"
-    // ktlint linter - read more: https://github.com/JLLeitschuh/ktlint-gradle
-    id("org.jlleitschuh.gradle.ktlint") version "10.0.0"
 }
 
 group = properties("pluginGroup")
